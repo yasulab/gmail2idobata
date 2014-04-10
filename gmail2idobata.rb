08ee721e50b0c61e0083fe4176ee9e961b8bb095
Yohei YASUKAWA <yasulab@gmail.com>
2014-04-10 12:31:24 +0900

    Initial commit

---
+++ gmail2idobata.rb
@@ -0,0 +1,65 @@
+#!/usr/bin/env ruby
+#coding: utf-8
+
+require 'gmail' # for more info -> http://dcparker.github.com/ruby-gmail/
+require 'shellwords'
+
+Signal.trap(:INT){
+  puts "logout Gmail ..."
+  @gmail.logout if defined? @gmail
+  puts "loged out!"
+  exit
+}
+
+def attached_file_exist?(filename)
+  if File.exist?(filename)
+    puts "checked the given file '#{filename}' exists."
+  else
+    puts "it seems '#{filename}' doesn not exist."
+    puts "check if the file really exists on the given path."
+    exit
+  end
+end
+
+USERNAME     = ENV['GMAIL_USERNAME']
+PASSWORD     = ENV['GMAIL_PASSWORD']
+IDOBATA_END  = ENV['IDOBATA_END']
+
+# login, confirm, then send/cancel and logout
+@gmail = Gmail.new(USERNAME, PASSWORD)
+
+#emailsの引数には:all,:read,:unreadがある.
+mails = @gmail.inbox.emails(:all).each do |mail|
+  text = ""
+  is_html_format = false
+
+  #text  += "<li>件名:   #{mail.subject}</li>"
+  #text  += "<li>日付:   #{mail.date}</li>"
+  #text  += "<li>送信者: #{mail.from.first.to_a.first}</li>"
+  #text  += "<li>受信者: #{mail.to}</li>" # この情報はいらない？
+
+  #件名、日付、From、To、本文処理
+  if !mail.text_part && !mail.html_part
+    text  += mail.body.decoded.encode("UTF-8", mail.charset)
+  elsif mail.html_part
+    text  += mail.html_part.decoded
+    is_html_format = true
+  elsif mail.text_part
+    text  += mail.text_part.decoded
+  end
+
+  ##curl --data-urlencode "source=hello, world." IDOBATA_END
+  #post = text.gsub("\n", "").gsub("`", "").shellescape
+  #post = text.shellescape.gsub("\n", "").gsub("'", "")
+  post = text.gsub("\n", "").gsub("'", "\"")
+  puts post
+  puts is_html_format
+
+  if is_html_format
+    system("curl --data-urlencode 'source=#{post}' -d format=html #{IDOBATA_END}")
+  else
+    system("curl --data-urlencode 'source=#{post}' #{IDOBATA_END}")
+  end
+end
+
+@gmail.logout
